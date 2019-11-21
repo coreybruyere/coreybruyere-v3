bbbde8ec05a07e36fd636aacec070561babb9bde
Corey Bruyere <coreybruyere@ymail.com>
2019-11-21 10:13:51 -0800

    Trigger manual deploy

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-  siteTitle: 'GCN',
-  siteTitleAlt: 'GCN Gatsby Starter', // This allows an alternative site title for SEO schema.
-  publisher: 'Publisher named GCN', // Organization name used for SEO schema
+  siteTitle: 'Corey Bruyere',
+  siteTitleAlt: 'Corey Bruyere Gatsby Starter', // This allows an alternative site title for SEO schema.
+  publisher: 'Corey Bruyere', // Organization name used for SEO schema
   siteDescription:
     'A starter template to build amazing static websites with Gatsby, Contentful and Netlify',
   siteUrl: 'https://gcn.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
@@ -9,7 +9,7 @@ module.exports = {
   postsPerPage: 6, // Number of posts shown on paginated pages
   author: 'GCN User', // Author for RSS author segment and SEO schema
   authorUrl: 'https://gcn.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
-  userTwitter: '@twitter', // Change for Twitter Cards
+  userTwitter: '@coreybruyere', // Change for Twitter Cards
   shortTitle: 'GCN App', // Used for App manifest e.g. Mobile Home Screen
   shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
   shareImageWidth: 900, // Change to the width of your default share image
