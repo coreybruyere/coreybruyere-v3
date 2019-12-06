84619c2750a11c2ca0520d6efeed9d90e9705ce9
Corey Bruyere <coreybruyere@ymail.com>
2019-12-06 10:53:36 -0800

    Prep sticky header layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import Helmet from 'react-helmet'
+
 import favicon from '../images/favicon.ico'
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
 import config from '../utils/siteConfig'
+
 import Menu from '../components/Menu'
 import Footer from '../components/Footer'
+import Position from '../components/Position'
 
 const Template = ({ children }) => {
   return (
-    <div className="siteRoot">
+    <>
       <Helmet>
         <title>{config.siteTitle}</title>
         <meta charSet="utf-8" />
@@ -20,15 +23,17 @@ const Template = ({ children }) => {
 
       <ThemeProvider theme={theme}>
         <>
-          <div className="siteContent">
-            {/* <Menu /> */}
-            {children}
-          </div>
+          <Position position="relative">
+            <Menu />
+            <main id="main" role="main">
+              {children}
+            </main>
+          </Position>
           <Footer />
         </>
       </ThemeProvider>
       <GlobalStyle />
-    </div>
+    </>
   )
 }
 
