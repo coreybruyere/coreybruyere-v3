1cefb5e50e4da5580e78aa570282e1a02d9f0bfb
Corey Bruyere <coreybruyere@ymail.com>
2020-04-10 09:14:57 -0700

    Add ScrollGradient to right side of WorkCardList

--- src/components/WorkCardList.jsx
+++ src/components/WorkCardList.jsx
@@ -2,20 +2,64 @@ import React from 'react'
 import styled from '@emotion/styled'
 import { Flex, Box } from 'rebass'
 import { rem } from 'polished'
+import { useTheme } from 'emotion-theming'
 
-import ShowMore from '../components/ShowMore'
+import ScrollGradient from '../components/ScrollGradient'
+
+const Wrap = styled(Box)`
+  position: relative;
+`
 
 const Scroll = styled(Box)`
   overflow-x: auto;
 `
 
+const GradientStart = styled(ScrollGradient)`
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 1;
+`
+
+const GradientEnd = styled(ScrollGradient)`
+  position: absolute;
+  right: 0;
+  top: 0;
+  z-index: 1;
+`
+
 const List = styled(Flex)``
 
+const BlankItem = styled(Box)`
+  list-style-type: none;
+`
+
 const WorkCardList = ({ children, ...other }) => {
+  const theme = useTheme()
   return (
-    <Scroll {...other}>
-      <List as="ul">{children}</List>
-    </Scroll>
+    <Wrap {...other}>
+      <Scroll>
+        <GradientStart
+          width={'32px'}
+          height={'100%'}
+          rotate={180}
+          gradientFill={'red'}
+        />
+        <List as="ul">
+          {children}
+          <BlankItem as="li" ml={4} aria-hidden="true">
+            &nbsp;
+          </BlankItem>
+        </List>
+
+        <GradientEnd
+          width={'32px'}
+          height={'100%'}
+          rotate={0}
+          gradientFill={theme.colors.background}
+        />
+      </Scroll>
+    </Wrap>
   )
 }
 
