b2c1db6774446baa4f39038233e9485478812b06
Corey Bruyere <coreybruyere@ymail.com>
2020-05-12 17:41:14 -0700

    Contact page progress

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import styled from '@emotion/styled'
-
-import Input from './Input'
-
+import React, { useReducer, useState } from 'react'
+import { useDialogState, Dialog, DialogDisclosure } from 'reakit/Dialog'
 /*
   ⚠️ This is an example of a contact form powered with Netlify form handling.
   Be sure to review the Netlify documentation for more information:
@@ -18,110 +14,115 @@ const encode = data => {
     .join('&')
 }
 
-class ContactForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: '',
-      email: '',
-      message: '',
-      showModal: false,
-    }
+const initialContactState = {
+  name: '',
+  email: '',
+  message: '',
+}
+
+const contactReducer = (state, action) => {
+  if (action.type === 'reset') {
+    return initialContactState
   }
 
-  handleInputChange = event => {
-    const target = event.target
-    const value = target.value
-    const name = target.name
-    this.setState({
-      [name]: value,
-    })
+  const result = { ...state }
+  result[action.type] = action.value
+  return result
+}
+
+const ContactForm = () => {
+  const [state, dispatch] = useReducer(contactReducer, initialContactState)
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const dialog = useDialogState({ modal: isModalVisible })
+
+  const handleInputChange = event => {
+    const { name, value } = event.target
+    dispatch({ type: name, value })
   }
 
-  handleSubmit = event => {
-    fetch('/?no-cache=1', {
+  const handleSubmit = event => {
+    console.log({ ...state })
+    fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'contact', ...this.state }),
+      body: encode({ 'form-name': 'contact', ...state }),
     })
-      .then(this.handleSuccess)
+      .then(console.log('success'))
       .catch(error => alert(error))
     event.preventDefault()
   }
 
-  handleSuccess = () => {
-    this.setState({
-      name: '',
-      email: '',
-      message: '',
-      showModal: true,
-    })
+  const handleSuccess = () => {
+    dispatch({ type: 'reset' })
+    console.log('handle success is running')
+    setIsModalVisible(true)
   }
 
-  closeModal = () => {
-    this.setState({ showModal: false })
+  const handleModalClose = () => {
+    setIsModalVisible(false)
   }
 
-  render() {
-    return (
-      <form
-        name="contact"
-        onSubmit={this.handleSubmit}
-        data-netlify="true"
-        data-netlify-honeypot="bot"
-        overlay={this.state.showModal}
-        onClick={this.closeModal}
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <p hidden>
-          <label>
-            Don’t fill this out:{' '}
-            <input name="bot" onChange={this.handleInputChange} />
-          </label>
-        </p>
+  return (
+    <form
+      name="contact"
+      data-netlify="true"
+      data-netlify-honeypot="bot"
+      onSubmit={handleSubmit}
+      onClick={handleModalClose}
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <p hidden>
+        <label>
+          Don’t fill this out: <input name="bot" onChange={handleInputChange} />
+        </label>
+      </p>
 
-        <Input
-          name="name"
-          type="text"
-          placeholder="Full Name"
-          value={this.state.name}
-          onChange={this.handleInputChange}
-          required
-        />
-        <Input
-          name="email"
-          type="email"
-          placeholder="Email"
-          value={this.state.email}
-          onChange={this.handleInputChange}
-          required
-        />
-        <textarea
-          name="message"
-          type="text"
-          placeholder="Message"
-          value={this.state.message}
-          onChange={this.handleInputChange}
-          required
-        />
-        <button name="submit" type="submit">
-          Send
-        </button>
+      <input
+        name="name"
+        type="text"
+        placeholder="Full Name"
+        value={state.name}
+        onChange={handleInputChange}
+        required
+      />
+      <input
+        name="email"
+        type="email"
+        placeholder="Email"
+        value={state.email}
+        onChange={handleInputChange}
+        required
+      />
+      <textarea
+        name="message"
+        type="text"
+        placeholder="Message"
+        value={state.message}
+        onChange={handleInputChange}
+        required
+      />
+      <button name="submit" type="submit">
+        Send
+      </button>
 
-        {/* <Modal visible={this.state.showModal}>
+      {/* <Modal visible={this.state.showModal}>
           <p>
             Thank you for reaching out. I will get back to you as soon as
             possible.
           </p>
           <Button onClick={this.closeModal}>Okay</Button>
         </Modal> */}
-      </form>
-    )
-  }
-}
 
-ContactForm.propTypes = {
-  data: PropTypes.object,
+      <Dialog
+        {...dialog}
+        aria-label="Welcome"
+        style={{ position: 'static', transform: 'none' }}
+      >
+        Focus is not trapped within me.
+        <button onClick={handleModalClose}>Close</button>
+      </Dialog>
+    </form>
+  )
 }
 
 export default ContactForm
