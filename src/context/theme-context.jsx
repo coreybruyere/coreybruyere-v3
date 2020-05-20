6199016ce27f101628c487f9ed01a5ca4ad51f85
Corey Bruyere <coreybruyere@ymail.com>
2020-05-20 09:40:05 -0700

    Add logs for build to debug

--- src/context/theme-context.jsx
+++ src/context/theme-context.jsx
@@ -10,6 +10,8 @@ const ThemeProvider = ({ children }) => {
   const storedThemeValue =
     typeof window !== 'undefined' ? localStorage.getItem('theme') : null
 
+  console.log(`stored theme value from provider: ${storedThemeValue}`)
+
   const [themeString, setThemeString] = useState(storedThemeValue || 'light')
   const lightTheme = { ...theme, colors: { ...lightColors } }
   const darkTheme = { ...theme, colors: { ...darkColors } }
@@ -24,13 +26,17 @@ const ThemeProvider = ({ children }) => {
 
 function useTheme() {
   const context = useContext(ThemeContext)
+  console.log(`CONTEXT from useTheme: ${context}`)
   if (!context) throw new Error('useTheme must be used within a ThemeProvider')
   const { themeString, setThemeString } = context
   const toggleTheme = useCallback(() => {
     if (themeString === 'light') {
+      console.log(`if themeString = light set storage dark`)
+
       window.localStorage.setItem('theme', 'dark')
       setThemeString('dark')
     } else if (themeString === 'dark') {
+      console.log(`if themeString = dark set storage light`)
       window.localStorage.setItem('theme', 'light')
       setThemeString('light')
     }
