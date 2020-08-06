755bb4542b22548e2869cd1ca2f6b92f7fdf335d
Corey Bruyere <coreybruyere@ymail.com>
2020-08-06 10:18:07 -0700

    Finish Modal - Probably need to test more but YOLO for now

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,20 +1,17 @@
 import React, { useReducer, useState, useRef } from 'react'
-import { Box } from 'rebass'
+import { Box, Text } from 'rebass'
 
 import Input from './Input'
 import TextArea from './TextArea'
 import Button from './Button'
 import Label from './Label'
-import Modal, { useModalState } from './Modal'
-import Test from './Test'
+import Modal from './Modal'
 
 /*
   ⚠️ This is an example of a contact form powered with Netlify form handling.
   Be sure to review the Netlify documentation for more information:
   https://www.netlify.com/docs/form-handling/
 */
-
-// @todo: CONVERT TO FUNCTIONAL COMPONENT AND ADD MODAL
 
 const encode = data => {
   return Object.keys(data)
@@ -40,17 +37,25 @@ const contactReducer = (state, action) => {
 
 const ContactForm = () => {
   const [state, dispatch] = useReducer(contactReducer, initialContactState)
-  const [isModalVisible, setIsModalVisible] = useState(true)
-  const modal = useModalState({ visible: isModalVisible })
+  const [isModalVisible, setIsModalVisible] = useState(false)
 
   const handleInputChange = event => {
     const { name, value } = event.target
     dispatch({ type: name, value })
   }
 
+  const handleSuccess = () => {
+    dispatch({ type: 'reset' })
+    setIsModalVisible(true)
+  }
+
+  const handleModalClose = () => {
+    setIsModalVisible(false)
+    dispatch({ type: 'reset' })
+  }
+
   const handleSubmit = event => {
     const form = event.target
-    console.log({ ...state })
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
@@ -61,92 +66,79 @@ const ContactForm = () => {
     event.preventDefault()
   }
 
-  const handleSuccess = () => {
-    dispatch({ type: 'reset' })
-    setIsModalVisible(true)
-  }
-
-  const handleModalClose = () => {
-    setIsModalVisible(false)
-  }
-
   return (
-    <form
-      name="contact-me"
-      method="post"
-      action="/contact-success/"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-      onSubmit={handleSubmit}
-    >
-      <Input type="hidden" name="form-name" value="contact-me" />
-      <p hidden>
-        <label>
-          Don’t fill this out:{' '}
-          <Input name="bot-field" onChange={handleInputChange} />
-        </label>
-      </p>
+    <>
+      <form
+        name="contact-me"
+        method="post"
+        action="/contact-success/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        <Input type="hidden" name="form-name" value="contact-me" />
+        <p hidden>
+          <label>
+            Don’t fill this out:{' '}
+            <Input name="bot-field" onChange={handleInputChange} />
+          </label>
+        </p>
+        <Box>
+          <Label htmlFor="contact-name">Name</Label>
+          <Input
+            name="name"
+            id="contact-name"
+            type="text"
+            placeholder="Your name"
+            value={state.name}
+            onChange={handleInputChange}
+            required
+          />
+        </Box>
+        <Box>
+          <Label htmlFor="contact-email">Email</Label>
+          <Input
+            name="email"
+            id="contact-email"
+            type="email"
+            placeholder="Your email address"
+            value={state.email}
+            onChange={handleInputChange}
+            required
+          />
+        </Box>
+        <Box>
+          <Label htmlFor="contact-message">Message</Label>
+          <TextArea
+            name="message"
+            id="contact-message"
+            type="text"
+            placeholder="Your message"
+            value={state.message}
+            onChange={handleInputChange}
+            required
+          />
+        </Box>
+        <Button name="submit" type="submit">
+          Send
+        </Button>
+      </form>
 
-      <Box>
-        <Label htmlFor="contact-name">Name</Label>
-        <Input
-          name="name"
-          id="contact-name"
-          type="text"
-          placeholder="Your name"
-          value={state.name}
-          onChange={handleInputChange}
-          required
-        />
-      </Box>
-
-      <Box>
-        <Label htmlFor="contact-email">Email</Label>
-        <Input
-          name="email"
-          id="contact-email"
-          type="email"
-          placeholder="Your email address"
-          value={state.email}
-          onChange={handleInputChange}
-          required
-        />
-      </Box>
-
-      <Box>
-        <Label htmlFor="contact-message">Message</Label>
-        <TextArea
-          name="message"
-          id="contact-message"
-          type="text"
-          placeholder="Your message"
-          value={state.message}
-          onChange={handleInputChange}
-          required
-        />
-      </Box>
-      <Button name="submit" type="submit">
-        Send
-      </Button>
-
-      <Modal.Trigger as={Button} {...modal}>
-        Open modal
-      </Modal.Trigger>
-
-      <Modal {...modal} ariaLabel="example" onClose={handleModalClose}>
-        Praesent sit amet quam ac velit faucibus dapibus. Quisque sapien ligula,
-        rutrum quis aliquam nec, convallis sit amet erat. Mauris auctor blandit
-        porta. In imperdiet rutrum nunc. Integer suscipit sodales ex, ut
-        lobortis orci rutrum id. Vestibulum scelerisque, felis ut sollicitudin
-        elementum, dolor nibh faucibus orci, eu aliquet felis diam sed eros.
-        Donec eget sapien lacinia, viverra felis in, placerat urna. Vestibulum
-        sed viverra orci. Donec id tellus eget dui porta lobortis ac eu metus.
-        Praesent id ultricies odio. In hac habitasse platea dictumst. Sed lorem
-        lacus, hendrerit non sodales id, consectetur quis magna. Nullam non
-        lacinia risus, ut varius est. Nam nec pulvinar tellus, eu ultrices elit.
-        Cras tincidunt et purus eu condimentum. Nunc vitae consequat nibh.
+      <Modal
+        open={isModalVisible}
+        onClose={handleModalClose}
+        label="Contact success"
+      >
+        <Text as="h1" fontSize={4} mb={2}>
+          Success!
+        </Text>
+        <Text as="p" mb={4}>
+          Thanks for connecting! Your message was sent my way and I'll be in
+          touch <abbr title="as soon as possible">ASAP</abbr>.
+        </Text>
+        <Button onClick={handleModalClose}>Dismiss</Button>
       </Modal>
-    </form>
+    </>
   )
 }
 
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,22 +1,9 @@
-/** @jsx jsx */
 import React from 'react'
 import styled from '@emotion/styled'
-import { css, keyframes, jsx } from '@emotion/core'
+import { keyframes } from '@emotion/core'
 import { rem } from 'polished'
-import {
-  DialogDisclosure,
-  useDialogState,
-  DialogBackdrop,
-  Dialog as ReakitDialog,
-} from 'reakit/Dialog'
-
-export function useModalState(options) {
-  return useDialogState({ animated: true, ...options })
-}
-
-export const cardStyle = css`
-  box-sizing: border-box;
-`
+import { DialogContent, DialogOverlay } from '@reach/dialog'
+import { Box } from 'rebass'
 
 const MASK_OVERLAY_SPACE = 32
 const DEFAULT_MODAL_WIDTH = 480
@@ -37,7 +24,7 @@ const fadeIn = keyframes`
     }
 `
 
-const Mask = styled(DialogBackdrop)`
+const Mask = styled(DialogOverlay)`
   position: fixed;
   top: 0;
   right: 0;
@@ -48,19 +35,19 @@ const Mask = styled(DialogBackdrop)`
   overflow: auto;
   background-color: rgba(0, 0, 0, 0.3);
   animation: ${fadeIn} 0.3s ease;
-  z-index: 99;
 `
 
-const ModalCard = styled(ReakitDialog)`
-  ${cardStyle};
+const ModalCard = styled(DialogContent)`
   position: fixed;
-  top: 50%;
+  top: 40%;
   left: 50%;
   width: calc(100% - ${rem(MASK_OVERLAY_SPACE)});
   max-width: ${props =>
     props.maxwidth ? rem(props.maxwidth) : rem(DEFAULT_MODAL_WIDTH)};
   max-height: calc(100vh - ${rem(MASK_OVERLAY_SPACE)});
-  overflow: unset;
+  background-color: ${({ theme }) => theme.colors.card};
+  box-shadow: 0 6px 12px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 0.25rem;
   animation: ${appear} 0.3s ease;
   animation-fill-mode: forwards;
   opacity: 0;
@@ -68,28 +55,11 @@ const ModalCard = styled(ReakitDialog)`
 `
 
 const Modal = props => {
-  const {
-    ariaLabel,
-    children,
-    hide,
-    hideOnClickOutside = true,
-    onClose,
-    ...other
-  } = props
-
-  function closeModal() {
-    if (onClose) onClose()
-    hide()
-  }
+  const { open, onClose, width, children, closeOnEsc, label, ...other } = props
   return (
-    <Mask {...other} hideOnClickOutside={hideOnClickOutside}>
-      <ModalCard
-        aria-label={ariaLabel}
-        hide={closeModal}
-        hideOnClickOutside={hideOnClickOutside}
-        {...other}
-      >
-        {children}
+    <Mask isOpen={open} onDismiss={onClose} {...other}>
+      <ModalCard maxwidth={width} aria-label={label}>
+        <Box p={3}>{children} </Box>
       </ModalCard>
     </Mask>
   )
@@ -100,5 +70,4 @@ Modal.defaultProps = {
   closeOnEsc: true,
 }
 
-Modal.Trigger = DialogDisclosure
 export default Modal
